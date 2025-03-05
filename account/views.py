b4f2d92fcabb33bc26ce3cb3cff3268329e331dd
Emanuel Angelo <emanuelangelo@outlook.com.br>
2025-03-05 11:36:22 -0300

    templates e views de login

--- account/views.py
+++ account/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Autenticado com sucesso.')
+                else:
+                    return HttpResponse('Conta desativada.')
+            else:
+                return HttpResponse('Login inválido.')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
+
+    
