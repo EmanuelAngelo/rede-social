5468cb00af1d31713e42490482eab43fc1283445
Emanuel Angelo <emanuelangelo@outlook.com.br>
2025-03-21 11:11:45 -0300

    att

--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.shortcuts import redirect
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('account/', include('account.urls')),
-    path('', lambda request: redirect('account/login/', permanent=True)), #redireciona para a página de login
-]
